f92b47de8f7aeb2fffa4e818ad5699526a40f85f
Ali <a.ozcan98@hotmail.com>
2025-03-20 17:58:45 +0100

    init frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TheHeader from './components/TheHeader.vue'
+</script>
+
+<template>
+  <TheHeader />
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding-top: 5vh;
+  max-width: 768px;
+  margin: 0 auto;
+}
+</style>
---
+++ frontend/src/components/TheHeader.vue
@@ -0,0 +1,45 @@
+<script lang="ts"></script>
+<template>
+  <header class="container">
+    <RouterLink to="/">
+      <div class="title">My<span style="color: var(--primary-clr)">Tasks</span></div>
+    </RouterLink>
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">New</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+nav {
+  display: flex;
+  gap: 1.1rem;
+}
+
+.title {
+  font-weight: bold;
+  font-size: 2rem;
+  cursor: pointer;
+}
+.title:hover {
+  /* opacity: 0.8; */
+  border-bottom: 2px solid var(--primary-clr);
+}
+
+a {
+  color: var(--light-clr);
+  text-decoration: none;
+}
+a:hover {
+  color: var(--primary-clr);
+}
+</style>
